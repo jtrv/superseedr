fad1f533d947094a39b6d80ff952048656279bff
Jaga Tranvo <jagatranvo@gmail.com>
2025-10-24 15:38:40 -0400

    fmt

--- src/tui_events.rs
+++ src/tui_events.rs
@@ -44,7 +44,9 @@ pub async fn handle_event(event: CrosstermEvent, app: &mut App) {
         {
             let mut help_key_handled = false;
             if app.app_state.show_help {
-                if key.code == KeyCode::Esc || (key.code == KeyCode::Char('m') && key.kind == KeyEventKind::Release) {
+                if key.code == KeyCode::Esc
+                    || (key.code == KeyCode::Char('m') && key.kind == KeyEventKind::Release)
+                {
                     app.app_state.show_help = false;
                     help_key_handled = true;
                 }
